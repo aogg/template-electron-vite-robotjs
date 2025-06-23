170e4de11e88af341243a15e862935ce14686287
ao-lol <355014+ao-lol@user.noreply.gitee.com>
2025-06-23 11:39:48 +0800

    devtool

--- src/main/index.js
+++ src/main/index.js
@@ -48,6 +48,9 @@ function createWindow() {
   } else {
     mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
   }
+
+  mainWindow.webContents.openDevTools();
+
 }
 
 // This method will be called when Electron has finished
